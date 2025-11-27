65ea17cc5775480a7f05a3812c22f38daab00a30
Marcel Khan <85454611+marcelkhan@users.noreply.github.com>
2025-11-27 19:36:22 +0100

    InterviewsNode

---
+++ Structured_Knowledge_Graph/GraphStructure/InterviewsSubGraph.py
@@ -0,0 +1,17 @@
+# Created by Marcel Khan on 27.11.25.
+from GraphModel.Graph import Graph
+from GraphModel.Nodes.NodeKnowledge import NodeKnowledge
+from Structured_Knowledge_Graph.GraphContent.Interviews import InterviewsNodeData
+
+
+class InterviewsSubGraph:
+    interviews_node: NodeKnowledge
+
+    def __init__(self, graph: Graph):
+        self.build_graph(graph)
+
+    def build_graph(self, graph: Graph):
+        self.interviews_node = NodeKnowledge(titel=InterviewsNodeData.TITEL,
+                                            description=InterviewsNodeData.CONTENT)
+
+        graph.add_new_node_to_graph(self.interviews_node)
--- Structured_Knowledge_Graph/StructuredKnowledgeGraph.py
+++ Structured_Knowledge_Graph/StructuredKnowledgeGraph.py
@@ -1,6 +1,7 @@
 # Created by Marcel Khan on 27.11.25.
 from GraphModel.Graph import Graph
 from Structured_Knowledge_Graph.GraphStructure.EvaluationSubGraph import EvaluationSubGraph
+from Structured_Knowledge_Graph.GraphStructure.InterviewsSubGraph import InterviewsSubGraph
 from Structured_Knowledge_Graph.GraphStructure.PersonalExperiencesSubGraph import PersonalExperiencesSubGraph
 
 
@@ -8,6 +9,7 @@ class StructuredKnowledgeGraph:
 
     evaluation_subgraph: EvaluationSubGraph
     personal_experiences_subgraph: PersonalExperiencesSubGraph
+    interviews_subgraph: InterviewsSubGraph
 
     def __init__(self, graph: Graph):
         self.assemble_graph(graph)
@@ -16,8 +18,12 @@ class StructuredKnowledgeGraph:
     def assemble_graph(self, graph):
         self.evaluation_subgraph = EvaluationSubGraph(graph)
         self.personal_experiences_subgraph = PersonalExperiencesSubGraph(graph)
+        self.interviews_subgraph = InterviewsSubGraph(graph)
 
     def connect_sub_graphs(self):
         self.evaluation_subgraph.evaluation_node.connect(
             self.personal_experiences_subgraph.personal_experiences_node
+        )
+        self.evaluation_subgraph.evaluation_node.connect(
+            self.interviews_subgraph.interviews_node
         )
