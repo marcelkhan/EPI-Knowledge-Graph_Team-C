5ef53eae709e857a63090318f3a24cc8c6f3fd45
Marcel Khan <85454611+marcelkhan@users.noreply.github.com>
2025-11-28 15:14:16 +0100

    NodeData

--- Structured_Knowledge_Graph/GraphStructure/InterviewsSubGraph.py
+++ Structured_Knowledge_Graph/GraphStructure/InterviewsSubGraph.py
@@ -1,7 +1,10 @@
 # Created by Marcel Khan on 27.11.25.
+
 from GraphModel.Graph import Graph
+from GraphModel.Nodes.NodeIllustration import NodeIllustration
 from GraphModel.Nodes.NodeKnowledge import NodeKnowledge
 from Structured_Knowledge_Graph.GraphContent.Interviews import InterviewsNodeData
+from Structured_Knowledge_Graph.GraphContent.KeepsDropsTrys import KeepsNodeData
 
 
 class InterviewsSubGraph:
@@ -14,4 +17,10 @@ class InterviewsSubGraph:
         self.interviews_node = NodeKnowledge(titel=InterviewsNodeData.TITEL,
                                             description=InterviewsNodeData.CONTENT)
 
+        keeps_node = NodeIllustration(titel=KeepsNodeData.TITLE,
+                                      image_name=KeepsNodeData.IMAGE_NAME)
+
+        self.interviews_node.connect(keeps_node)
+
         graph.add_new_node_to_graph(self.interviews_node)
+        graph.add_new_node_to_graph(keeps_node)
