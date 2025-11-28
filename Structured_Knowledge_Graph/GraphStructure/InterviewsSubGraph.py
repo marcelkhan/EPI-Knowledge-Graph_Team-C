ca408e4fa2098eb1fe68e5444445c5cd22b48910
Marcel Khan <85454611+marcelkhan@users.noreply.github.com>
2025-11-28 15:24:15 +0100

    Nodes created and added

--- Structured_Knowledge_Graph/GraphStructure/InterviewsSubGraph.py
+++ Structured_Knowledge_Graph/GraphStructure/InterviewsSubGraph.py
@@ -4,7 +4,7 @@ from GraphModel.Graph import Graph
 from GraphModel.Nodes.NodeIllustration import NodeIllustration
 from GraphModel.Nodes.NodeKnowledge import NodeKnowledge
 from Structured_Knowledge_Graph.GraphContent.Interviews import InterviewsNodeData
-from Structured_Knowledge_Graph.GraphContent.KeepsDropsTrys import KeepsNodeData
+from Structured_Knowledge_Graph.GraphContent.KeepsDropsTrys import KeepsNodeData, DropsNodeData, TrysNodeData
 
 
 class InterviewsSubGraph:
@@ -19,8 +19,16 @@ class InterviewsSubGraph:
 
         keeps_node = NodeIllustration(titel=KeepsNodeData.TITLE,
                                       image_name=KeepsNodeData.IMAGE_NAME)
+        drops_node = NodeIllustration(titel=DropsNodeData.TITLE,
+                                      image_name=DropsNodeData.IMAGE_NAME)
+        trys_node = NodeIllustration(titel=TrysNodeData.TITLE,
+                                     image_name=TrysNodeData.IMAGE_NAME)
 
         self.interviews_node.connect(keeps_node)
+        self.interviews_node.connect(drops_node)
+        self.interviews_node.connect(trys_node)
 
         graph.add_new_node_to_graph(self.interviews_node)
         graph.add_new_node_to_graph(keeps_node)
+        graph.add_new_node_to_graph(drops_node)
+        graph.add_new_node_to_graph(trys_node)
--- Structured_Knowledge_Graph/GraphStructure/PersonalExperiencesSubGraph.py
+++ Structured_Knowledge_Graph/GraphStructure/PersonalExperiencesSubGraph.py
@@ -1,6 +1,8 @@
 # Created by Marcel Khan on 27.11.25.
 from GraphModel.Graph import Graph
+from GraphModel.Nodes.NodeIllustration import NodeIllustration
 from GraphModel.Nodes.NodeKnowledge import NodeKnowledge
+from Structured_Knowledge_Graph.GraphContent.KeepsDropsTrys import KeepsNodeData, DropsNodeData, TrysNodeData
 from Structured_Knowledge_Graph.GraphContent.PersonalExperiences import PersonalExperiencesNodeData
 
 
@@ -15,4 +17,18 @@ class PersonalExperiencesSubGraph:
         self.personal_experiences_node = NodeKnowledge(titel=PersonalExperiencesNodeData.TITEL,
                                                       description=PersonalExperiencesNodeData.CONTENT)
 
+        keeps_node = NodeIllustration(titel=KeepsNodeData.TITLE,
+                                      image_name=KeepsNodeData.IMAGE_NAME)
+        drops_node = NodeIllustration(titel=DropsNodeData.TITLE,
+                                      image_name=DropsNodeData.IMAGE_NAME)
+        trys_node = NodeIllustration(titel=TrysNodeData.TITLE,
+                                     image_name=TrysNodeData.IMAGE_NAME)
+
+        self.personal_experiences_node.connect(keeps_node)
+        self.personal_experiences_node.connect(drops_node)
+        self.personal_experiences_node.connect(trys_node)
+
         graph.add_new_node_to_graph(self.personal_experiences_node)
+        graph.add_new_node_to_graph(keeps_node)
+        graph.add_new_node_to_graph(drops_node)
+        graph.add_new_node_to_graph(trys_node)
