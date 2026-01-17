bcb984c3dce02cc0f663ac6f31ada51bed85366d
fluzeman <dretzte12>
2026-01-17 19:28:34 +0100

    Add Picture LearningMethodsGraph.PNG + Node and Results

---
+++ Structured_Knowledge_Graph/GraphContent/Interviews/Evaluations/LearningMethodsNodeData.py
@@ -0,0 +1,8 @@
+"""
+Auswertungen aus der Umfrage von
+"Evaluation des Studiengangs Informatik (B.A.) - Studentische Perspektiven für die Reakkreditierung an der TH-Köln"
+"""
+
+TITEL = " Welche Unterstützungen haben dir bisher besonders in deinem Studium geholfen? "
+
+IMAGE_NAME = "LearningMethodsGraph.PNG"
---
+++ Structured_Knowledge_Graph/GraphContent/Interviews/Evaluations/LearningMethodsResultsNodeData.py
@@ -0,0 +1,24 @@
+"""
+InterviewsNodeData definiert die Inhalte für die Befragungen von Mitstudierenden.
+Diese Befragungen sollen Einblicke in die Erfahrungen und Meinungen anderer Studierender geben.
+
+Dieser Knoten enthält die Auswertung des Diagrams zu der Frage:
+"Welche Unterstützungen haben dir bisher besonders in deinem Studium geholfen?"
+"""
+TITEL = "Welche Unterstützungen haben dir bisher besonders in deinem Studium geholfen?"
+
+CONTENT = ("### Auswertung des Diagramms: Welche Unterstützungen haben dir bisher besonders in deinem Studium geholfen?"
+           "Die Auswertung der Frage zeigt, welche Unterstützungsangebote von den Studierenden als besonders hilfreich wahrgenommen werden. "
+           "Insgesamt wurden sieben Antworten abgegeben, wobei sich eine klare Schwerpunktsetzung erkennen lässt. \n\n"
+           "Den größten Anteil nehmen Praktika ein, die mit 4 Bewertungen ~57 % mehr als die Hälfte der Nennungen ausmachen. "
+           "An zweiter Stelle folgen Workshops mit 2 Bewertungen ~28 %, während Videos mit Bewertungen ~14 % ebenfalls einen relevanten, wenn auch deutlich geringeren Anteil haben. "
+           "Andere Unterstützungsformen wie Tutorien, Mentoring oder Lerngruppen haben alle 0 Bewertungen erhalten.\n\n"
+           "### Interpretation der Ergebnisse: \n"
+           "Die deutliche Dominanz der Praktika verdeutlicht, dass angewendete Erfahrungen starken Stellenwert im Studium haben."
+           "Studierende profitieren offenbar stark von der Möglichkeit, aufgaben in einem zeitlich begrenzten Zeitraum zu bearbeiten und dafür schnelles Feedback zu bekommen.\n\n"
+           "Auch die vergleichsweise hohe Bewertung von Workshops spricht für den Wunsch nach interaktiven und strukturierten Lernformaten, "
+           "die gezielt bestimmte Fähigkeiten oder Themen vertiefen. Workshops bieten häufig einen klaren Praxisbezug und ermöglichen aktives Mitwirken, "
+           "was von vielen Studierenden als lernförderlich empfunden wird.\n\n"
+           "Die fehlenden Nennungen bei Tutorien, Mentoring und Lerngruppen sind dabei auch im Zusammenhang mit der geringen Anzahl an Teilnehmenden zu betrachten. "
+           "Die Nullbewertungen lassen daher nicht zwangsläufig auf eine geringe Qualität oder fehlenden Nutzen dieser Formate schließen. \n\n"
+           "### Autor: Florian Luzemann\n")
