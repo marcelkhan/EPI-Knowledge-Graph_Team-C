f4fe3d8adea0cb684bdcda2db9289ce71eacdd27
Marcel Khan <85454611+marcelkhan@users.noreply.github.com>
2026-01-17 15:16:06 +0100

    Update survey results in ExpectationsNodeData.py
    Expanded the CONTENT variable to include detailed responses from 7 participants regarding their expectations of the study program and whether these were met. Added author attribution and improved formatting for clarity.

--- Structured_Knowledge_Graph/GraphContent/Interviews/Evaluations/ExpectationsNodeData.py
+++ Structured_Knowledge_Graph/GraphContent/Interviews/Evaluations/ExpectationsNodeData.py
@@ -7,6 +7,21 @@ Auswertungen aus der Umfrage von
 
 TITEL = "Welche Erwartungen hattest du ins Studium und wurden diese bisher erfüllt?"
 
-CONTENT = ("In diesem Knoten werden die Ergebnisse der Befragung zur Frage der Erwartungen an das Studium und deren "
-           "Erfüllung gesammelt. "
-           "Antwort:")
+CONTENT = ("### In diesem Knoten werden die Ergebnisse der Befragung zur Frage der Erwartungen an das Studium und deren "
+           "Erfüllung gesammelt. \n"
+           "Insgesamt haben wir zu dieser Umfrage 7 Antworten erhalten. \n\n"
+           "### Antworten: \n"
+           "- Ich hatte die Erwartung zu lernen wie man Programme nach Anforderungen erstellt und dass ich weitere "
+           "Programmiersprachen außerhalb von Java lernen würde. \n"
+           "Das Erstellen von Programmen nach Anforderungen wurde mir gut vermittelt. Andere Programmiersprachen wurden "
+           "in Fächern zwar angeboten, jedoch fehlten mir hier "
+           "oftmals Einführungen in diese um mit ihnen arbeiten zu können. \n\n"
+           "- Ich habe ein Praxisnahes Studium erwartet und bis auf ein paar organisatorische Punkte bin ich mit meinem "
+           "Studium Sehr zufrieden! \n\n"
+           "- Ja, größtenteils. Ich fand IT-Security kommt ein bisschen zu kurz. \n\n"
+           "- Tieferes Verständnis der theoretischen Grundlagen sowie Erwerb praktischer Expertise – beides Erfüllt. \n\n"
+           "- Ich habe mich nach der Schule gefreut, mehr über die Informatik mit gleichgesinnten zu lernen und wurde "
+           "bisher nicht enttäuscht. \n\n"
+           "- Keine Erwartungen, aber positiv überrascht. \n\n"
+           "- diese wurden erfüllt. \n\n"
+           "### Autor: Marcel Khan\n")
