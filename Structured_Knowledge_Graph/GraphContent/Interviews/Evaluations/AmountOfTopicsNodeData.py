71dbb7a7f1ecfd709c479c60b65a2c328580eeae
fluzeman <dretzte12>
2026-01-18 00:36:13 +0100

    Add 5 Questions 1 try 1 keep 3 PNGs

---
+++ Structured_Knowledge_Graph/GraphContent/Interviews/Evaluations/AmountOfTopicsNodeData.py
@@ -0,0 +1,10 @@
+# Created by Timon Kirch on 17.01.26.
+
+"""
+Auswertungen aus der Umfrage von
+"Evaluation des Studiengangs Informatik (B.A.) - Studentische Perspektiven für die Reakkreditierung an der TH-Köln"
+"""
+
+TITEL = "Wie würdest du die Angebote in den jeweiligen Themenbereichen anhand der Menge bewerten?"
+
+IMAGE_NAME = "AmountOfTopics.png"
---
+++ Structured_Knowledge_Graph/GraphContent/Interviews/Evaluations/IncreaseOfLectureTypeResultsNodeData.py
@@ -0,0 +1,20 @@
+# Created by Timon Kirch on 17.01.2026
+
+"""
+Auswertungen aus der Umfrage von
+"Evaluation des Studiengangs Informatik (B.A.) - Studentische Perspektiven für die Reakkreditierung an der TH-Köln"
+"""
+
+TITEL = "Auswertung - Welche der folgenden Lehrformate würdest du dir mehr wünschen?"
+
+CONTENT = ("### Auswertung des Diagramms: Welche der folgenden Lehrformate würdest du dir mehr wünschen? \n\n"
+           "###NotFinished\n\n"
+           "In unserer Umfrage wurden die Studenten auch zu Lehrformaten befragt. Hierbei konnten sie zwischen den "
+           "Lehrformaten ‚Projekte‘, ‚Gastvorträge‘, ‚Problem Based Learning (reale Probleme annähern und lösen)‘, "
+           "‚Gamification (Interaktives Spielen und Lernen)‘ und ‚Podcasts / Video von Profs zu Themen (ArchiLab)‘ "
+           "wählen und sollten angeben, von welchem dieser Lehrformate sie sich mehr wünschen würden. Ein Siebtel der "
+           "Befragten gab an, dass sie sich mehr ‚Podcasts / Videos von Profs zu Themen (ArchiLab)‘ wünschen würden. "
+           "Drei Siebtel wünschten sich mehr ‚Problem Based Learning (reale Probleme annähern und lösen)‘. Drei "
+           "Siebtel der Befragten gaben an, sich mehr ‚Gamification (interaktives Spielen und Lernen)‘ zu wünschen."
+           "\n\n"
+           "### Autor: Timon Kirch\n")
