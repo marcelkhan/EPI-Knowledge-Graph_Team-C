7cee328a07add4de42433ed1ce9f573b3cbe7bc6
Marcel Khan <85454611+marcelkhan@users.noreply.github.com>
2026-01-17 15:55:32 +0100

    Add and integrate ExpectationsKeepsNodeData for interviews
    Added ExpectationsKeepsNodeData and ExpectationsDropsNodeData modules for interview content. Extended ExpectationsNodeData with a qualitative summary. Updated InterviewsKeepsSubGraph to include and connect the new ExpectationsKeepsNodeData node, enhancing the representation of student expectations in the knowledge graph.

--- Structured_Knowledge_Graph/GraphStructure/Interviews/InterviewsKeepsSubGraph.py
+++ Structured_Knowledge_Graph/GraphStructure/Interviews/InterviewsKeepsSubGraph.py
@@ -1,6 +1,8 @@
 # Created by Marcel Khan on 14.01.26.
 from GraphModel.Graph import Graph
 from GraphModel.Nodes.NodeIllustration import NodeIllustration
+from GraphModel.Nodes.NodeKnowledge import NodeKnowledge
+from Structured_Knowledge_Graph.GraphContent.Interviews.Keeps import ExpectationsKeepsNodeData
 from Structured_Knowledge_Graph.GraphContent.KeepsDropsTrys import KeepsNodeData
 
 
@@ -14,4 +16,10 @@ class InterviewsKeepsSubGraph:
         self.interviews_keeps_node = NodeIllustration(titel=KeepsNodeData.TITLE,
                                                         image_name=KeepsNodeData.IMAGE_NAME)
 
+        expectations_keeps_node = NodeKnowledge(titel=ExpectationsKeepsNodeData.TITEL,
+                                                description=ExpectationsKeepsNodeData.CONTENT)
+
+        self.interviews_keeps_node.connect(expectations_keeps_node)
+
         graph.add_new_node_to_graph(self.interviews_keeps_node)
+        graph.add_new_node_to_graph(expectations_keeps_node)
